826eb60f514fa85a47e459f75a5246356674cc85
Umang Patel <umang85patel@hotmail.com>
2015-11-17 18:44:12 +0530

    all changes before diwali 2015.. started work after diwali from this version

--- app/src/main/java/in/wasure/wasurenew/FreshOrderActivity.java
+++ app/src/main/java/in/wasure/wasurenew/FreshOrderActivity.java
@@ -22,38 +22,38 @@ public class FreshOrderActivity extends AppCompatActivity {
 
         String itemList[]={"Pant", "Shirt", "T-Shirt", "Jeans", "Towel"};
 
-        for(int i=0;i<5;i++)
-        {
-            LayoutParams lparams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, 1.0f);
-            lparams.setMargins(0,0,0,20);
-
-            LinearLayout layout = new LinearLayout(this);
-            layout.setOrientation(LinearLayout.HORIZONTAL);
-            layout.setLayoutParams(lparams);
-
-            TextView itemName = new TextView(this);
-            itemName.setLayoutParams(lparams);
-            itemName.setText(itemList[i]);
-            layout.addView(itemName);
-
-            LayoutParams lparams2 = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, 2.0f);
-
-            Spinner spinner = new Spinner(this);
-            itemName.setLayoutParams(lparams2);
-            ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.nOfItems)); //selected item will look like a spinner set from XML
-            spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            spinner.setAdapter(spinnerArrayAdapter);
-            layout.addView(spinner);
-
-            Spinner spinner2 = new Spinner(this);
-            itemName.setLayoutParams(lparams);
-            ArrayAdapter<String> spinnerArrayAdapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.srvType)); //selected item will look like a spinner set from XML
-            spinnerArrayAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            spinner2.setAdapter(spinnerArrayAdapter2);
-            layout.addView(spinner2);
-
-            parentlayout.addView(layout);
-        }
+//        for(int i=0;i<5;i++)
+//        {
+//            LayoutParams lparams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, 1.0f);
+//            lparams.setMargins(0,0,0,20);
+//
+//            LinearLayout layout = new LinearLayout(this);
+//            layout.setOrientation(LinearLayout.HORIZONTAL);
+//            layout.setLayoutParams(lparams);
+//
+//            TextView itemName = new TextView(this);
+//            itemName.setLayoutParams(lparams);
+//            itemName.setText(itemList[i]);
+//            layout.addView(itemName);
+//
+//            LayoutParams lparams2 = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, 2.0f);
+//
+//            Spinner spinner = new Spinner(this);
+//            itemName.setLayoutParams(lparams2);
+//            ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.nOfItems)); //selected item will look like a spinner set from XML
+//            spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//            spinner.setAdapter(spinnerArrayAdapter);
+//            layout.addView(spinner);
+//
+//            Spinner spinner2 = new Spinner(this);
+//            itemName.setLayoutParams(lparams);
+//            ArrayAdapter<String> spinnerArrayAdapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.srvType)); //selected item will look like a spinner set from XML
+//            spinnerArrayAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//            spinner2.setAdapter(spinnerArrayAdapter2);
+//            layout.addView(spinner2);
+//
+//            parentlayout.addView(layout);
+//        }
 
 
     }
