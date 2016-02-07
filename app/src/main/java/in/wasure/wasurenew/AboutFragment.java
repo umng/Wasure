7b88ae6d05787bd449409754cf4b2749b87578da
Umang Patel <umang85patel@hotmail.com>
2016-02-08 00:57:32 +0530

    One Corporation link added and support page images quality improved

--- app/src/main/java/in/wasure/wasurenew/AboutFragment.java
+++ app/src/main/java/in/wasure/wasurenew/AboutFragment.java
@@ -1,17 +1,23 @@
 package in.wasure.wasurenew;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class AboutFragment extends Fragment {
     public AboutFragment() {
         // Required empty public constructor
     }
+
+    Button oneWeb;
+    String url;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,19 @@ public class AboutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_about, container, false);
+
+        url = getString(R.string.oneWebSite);
+        if (!url.startsWith("http://") && !url.startsWith("https://"))
+            url = "http://" + url;
+
+        oneWeb = (Button) rootView.findViewById(R.id.fragment_about_one);
+        oneWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(browserIntent);
+            }
+        });
 
 
         // Inflate the layout for this fragment
